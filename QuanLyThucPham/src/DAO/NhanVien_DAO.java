21e2e6461902817f7def7d5501837616dda5bea0
bibaocodekopro <95998397+bibaocodekopro@users.noreply.github.com>
2022-05-16 14:09:18 +0700

    Bao 2:09 16/5

--- QuanLyThucPham/src/DAO/NhanVien_DAO.java
+++ QuanLyThucPham/src/DAO/NhanVien_DAO.java
@@ -2,14 +2,44 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package DAO;
-
+package DAO;    
 import DTO.*;
-
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.*;
+import java.sql.*;
 /**
  *
  * @author blemb
  */
 public class NhanVien_DAO {
-    
+    public static Connection conn = null;
+    MyConnect mysql = new MyConnect();
+    public static ResultSet rs=null;
+    public static Statement st=null;
+  
+    public NhanVien_DAO(){
+        try {
+             conn = mysql.Open();
+                    } catch (SQLException ex) {
+            Logger.getLogger(NhanVien_DAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+  
+    public ResultSet DangNhap(String TenDangNhap, String MatKhau){
+        try {
+            st = conn.createStatement();
+            String query = "SELECT * FROM nhanvien where TenDangNhap='" + TenDangNhap + "' AND MatKhau='" + MatKhau + "'";
+            rs = st.executeQuery(query);
+            rs.next();
+            return rs;
+        } catch (Exception e) {
+        }
+         return rs=null;
+    }
 }
