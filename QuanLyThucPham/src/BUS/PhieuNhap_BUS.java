9116c19f193a0c9bcbc8b8db7ecc1eed812199c2
ITDauPh <phanvandau50@gmail.com>
2022-05-18 01:15:08 +0700

    Dau 1:15

--- QuanLyThucPham/src/BUS/PhieuNhap_BUS.java
+++ QuanLyThucPham/src/BUS/PhieuNhap_BUS.java
@@ -4,12 +4,20 @@
  */
 package BUS;
 
+
+import DAO.PhieuNhap_DAO;
 import DTO.*;
+import java.util.ArrayList;
 
 /**
  *
  * @author blemb
  */
 public class PhieuNhap_BUS {
+    public static ArrayList<PhieuNhap_DTO> dspn;
+    public PhieuNhap_BUS(){};
+    public void docDSPN(){
+        dspn = new PhieuNhap_DAO().docPN();
+    }
     
 }
