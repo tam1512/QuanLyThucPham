d01c3bb6e9ccaae4941a3c3746b2a6f2ecc95ca1
bibaocodekopro <95998397+bibaocodekopro@users.noreply.github.com>
2022-05-18 00:54:14 +0700

    Bao 12:53 18/5/2022
    NhanVien

--- QuanLyThucPham/src/BUS/NhanVien_BUS.java
+++ QuanLyThucPham/src/BUS/NhanVien_BUS.java
@@ -4,12 +4,27 @@
  */
 package BUS;
 
+import DAO.NhanVien_DAO;
 import DTO.*;
+import java.util.ArrayList;
 
 /**
  *
  * @author blemb
  */
 public class NhanVien_BUS {
-    
+     public static ArrayList<NhanVien_DTO> dsnv;
+    public NhanVien_BUS(){}
+     public void docNhanVien(){
+        dsnv = new NhanVien_DAO().docNhanVien();
+    }
+      public void docNhanVien_edit(){
+        dsnv = new NhanVien_DAO().docNhanVien_edit();
+    }
+     public void sua_NhanVien(String ID_NhanVien,String HoVaTen,String DiaChi,String Email,String SoDienThoai){
+         new NhanVien_DAO().suaNhanVien(ID_NhanVien, HoVaTen, DiaChi, Email, SoDienThoai);
+     }
+     public void xoa_NhanVien(String ID_NhanVien){
+         new NhanVien_DAO().xoa_NhanVien(ID_NhanVien);
+     }
 }
