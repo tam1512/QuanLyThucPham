a1a78d2a8a67e06263b890e4f5055b87efe0c0eb
ITDauPh <phanvandau50@gmail.com>
2022-05-18 01:46:19 +0700

    hhh
    hhh

--- QuanLyThucPham/src/DTO/ThanhVien_DTO.java
+++ QuanLyThucPham/src/DTO/ThanhVien_DTO.java
@@ -11,66 +11,19 @@ package DTO;
  */
 public class ThanhVien_DTO {
     public String ID_ThanhVien;
-    public String TenDangNhap;
-    public String MatKhau;
-    public String Email;
     public String HoVaTen;
-    public String DiaChi;
+    public String NgayDangKi;
     public String SoDienThoai;
     
     public ThanhVien_DTO(){
     
     }
-    public ThanhVien_DTO(String ID_ThanhVien,String TenDangNhap,String MatKhau,String Email,String HoVaTen,String DiaChi,String SoDienThoai){
+    public ThanhVien_DTO(String ID_ThanhVien,String HoVaTen,String SoDienThoai, String NgayDangKi){
         this.ID_ThanhVien=ID_ThanhVien;
-        this.TenDangNhap=TenDangNhap;
-        this.MatKhau=MatKhau;
-        this.Email=Email;
         this.HoVaTen=HoVaTen;
-        this.DiaChi=DiaChi;
         this.SoDienThoai=SoDienThoai;
+        this.NgayDangKi=NgayDangKi;
     }
-    public String getID_ThanhVien(){
-        return this.ID_ThanhVien;
-    }
-    public void setID_ThanhVien(String ID_ThanhVien){
-        this.ID_ThanhVien=ID_ThanhVien;
-    }
-    public String getTenDangNhap(){
-        return TenDangNhap;
-    }
-    public void setTenDangNhap(String TenDangNhap){
-        this.TenDangNhap=TenDangNhap;
-    }
-    public String getMatKhau(){
-        return MatKhau;
-    }
-    public void setMatKhau(String MatKhau){
-        this.MatKhau=MatKhau;   
-    }
-    public String getEmail(){
-        return Email;
-    }
-    public void setEmail(String Email){
-        this.Email=Email;
-    }
-    public String getHoVaTen(){
-        return HoVaTen;
-    }
-    public void setHoVaTen(String HoVaTen){
-        this.HoVaTen=HoVaTen;
-    }
-    public String getDiaChi(){
-        return this.DiaChi;
-    }
-    public void setDiaChi(String DiaChi){
-        this.DiaChi=DiaChi;
-    }
-    public String getSoDienThoai(){
-        return this.SoDienThoai;
-    }
-    public void setSoDienThoai(String SoDienThoai){
-        this.SoDienThoai=SoDienThoai;
-    }
+    
     
 }
